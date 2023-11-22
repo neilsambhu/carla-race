30f45c0b3e49dc9897335fea1b8896712bd6c889
Neilesh Sambhu <neilsambhu@gmail.com>
2023-11-22 11:16:31 -0500

    output VehicleControl done

--- run/PythonAPI/carla/agents/navigation/local_planner.py
+++ run/PythonAPI/carla/agents/navigation/local_planner.py
@@ -267,9 +267,58 @@ class LocalPlanner(object):
             control.manual_gear_shift = False
         else:
             self.target_waypoint, self.target_road_option = self._waypoints_queue[0]
-            if bVerbose:
+            if bVerbose and False:
                 print(f'self.target_waypoint: {self.target_waypoint}\tself.target_road_option: {self.target_road_option}')
             control = self._vehicle_controller.run_step(self._target_speed, self.target_waypoint)
+            if bVerbose and False:
+                print(f'control: {control}')
+            '''
+            # # 11/21/2023 10:45 PM: save control signal: start
+            # import json
+            # # temporary: save one control signal value: start
+            # def serialize_vehicle_control(vehicle_control):
+            #     return {
+            #         'throttle': vehicle_control.throttle,
+            #         'steer': vehicle_control.steer,
+            #         'brake': vehicle_control.brake,
+            #         'hand_brake': vehicle_control.hand_brake,
+            #         'reverse': vehicle_control.reverse,
+            #         'manual_gear_shift': vehicle_control.manual_gear_shift,
+            #         'gear': vehicle_control.gear,
+            #     }
+            # control_data = serialize_vehicle_control(control)
+            # with open('out_controlPickles/control.pkl', 'wb') as file:
+            #     json.dump(control_data, file)
+            # # temporary: save one control signal value: end
+            # # 11/21/2023 10:45 PM: save control signal: end
+            # # 11/22/2023 10:38 AM: save control signal: start: https://stackoverflow.com/a/25119089
+            # import dill
+            # with open('_out_control/control.pkl', 'wb') as file:
+            #     dill.dump(control, file)
+            # # 11/22/2023 10:38 AM: save control signal: end            
+            '''
+            # 11/22/2023 10:48 AM: save control signal: start
+            with open('_out_control/control.txt', 'w') as file:
+                file.write('{} {} {} {} {} {} {}'.format(
+                    control.throttle,
+                    control.steer,
+                    control.brake,
+                    control.hand_brake,
+                    control.reverse,
+                    control.manual_gear_shift,
+                    control.gear))
+            # 11/22/2023 10:48 AM: save control signal: end
+            # 11/22/2023 11:08 AM: output control signal: start
+            control = carla.VehicleControl(
+                throttle=1.0,
+                steer=0.5,
+                brake=0.0,
+                hand_brake=False,
+                reverse=False,
+                manual_gear_shift=False,
+                gear=0)
+            # 11/22/2023 11:08 AM: output control signal: end
+
 
         if debug:
             draw_waypoints(self._vehicle.get_world(), [self.target_waypoint], 1.0)
