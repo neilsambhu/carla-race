bd5fa0a616ed52521b7172e362579dbc7b288b9c
Neilesh Sambhu <neilsambhu@gmail.com>
2023-12-12 22:08:01 -0500

    save p4

--- run/2023_12_12_13tutorial.py
+++ run/2023_12_12_13tutorial.py
@@ -8,6 +8,9 @@ import cv2
 import math
 from collections import deque
 from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.models import Model
 
 
 try:
@@ -163,3 +166,70 @@ class DQNAgent:
         model = Model(inputs = base_model.input, outputs=predictions)
         model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=["accuracy"])
         return model
+
+    def update_replay_memory(self, transition):
+        # transition = (current_state, action, reward, new_state, done)
+        self.replay_memory.append(transition)
+
+    def train(self):
+        if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
+            return
+
+        minibatch = random.sample(self.replay_memory, MINIBATCH_SIZE)
+
+        current_states = np.array([transition[0] for transition in minibatch])/255
+        with self.graph.as_default():
+            current_qs_list = self.model.predit(current_states, PREDICTION_BATCH_SIZE)
+
+        new_current_states = np.array([transition[3] for transition in minibatch])/255
+        with self.graph.as_default():
+            future_qs_list = self.target_model.predit(new_current_states, PREDICTION_BATCH_SIZE)
+
+        X = []
+        y = []
+
+        for index, (current_state, action, reward, new_state, done) in enumerate(minibatch):
+            if not done:
+                max_future_q = np.max(future_qs_list[index])
+                new_q = reward + DISCOUNT * max_future_q
+            else:
+                new_q = reward
+
+            current_qs = current_qs_list[index]
+            current_qs[action] = new_q
+
+            X.append(current_state)
+            y.append(current_qs)
+
+        log_this_step = False
+        if self.tensorboard.step > self.last_logged_episode:
+            log_this_step = True
+            self.last_log_episode = self.tensorboard.step
+
+        with self.graph.as_default():
+            self.model.fit(np.array(X)/255, np.array(y), batch_size=TRAINING_BATCH_SIZE, verbose=0, shuffle=False, callbacks=[self.tensorboard] if log_this_step else None)
+
+
+        if log_this_step:
+            self.target_update_counter += 1
+
+        if self.target_update_counter > UPDATE_TARGET_EVERY:
+            self.target_model.set_weights(self.model.get_weights())
+            self.target_update_counter = 0
+
+    def get_qs(self, state):
+        return self.model.predict(np.array(state).reshape(-1 *state.shape)/255)[0]
+
+    def train_in_loop(self):
+        X = np.random.uniform(size=(1, IM_HEIGHT, IM_WIDTH, 3)).astype(np.float32)
+        y = np.random.uniform(size=(1, 3)).astype(np.flat32)
+        with self.graph.as_default():
+            self.model.fit(X,y, verbose=False, batch_size=1)
+
+        self.training_initialized = True
+
+        while True:
+            if self.terminate:
+                return
+            self.train()
+            time.sleep(0.01)
