38c5a98b459524242c4739ef79687629911eda85
Neil Sambhu <neilsambhu@gmail.com>
2023-12-31 15:40:02 -0500

    typo 'epoch' to 'batch'

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -633,7 +633,7 @@ if __name__ == "__main__":
             # print(f'agent.count_saved_models: {agent.count_saved_models}')
             
             agent.saved_model.save(f'tmp/{env.episode:03}.{agent.count_batches_trained}.model')
-            print(f'Saved model from episode {env.episode}. Count of epochs trained: {agent.count_batches_trained}')
+            print(f'Saved model from episode {env.episode}. Count of batches trained: {agent.count_batches_trained}')
 
             with open(f'tmp/{env.episode:03}.replay_memory', 'wb') as file:
                 pickle.dump(agent.replay_memory, file)
