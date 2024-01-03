46497fe2a39c4f005048ccbcdb7bd22391aeb82d
Neil Sambhu <neilsambhu@gmail.com>
2024-01-02 23:33:27 -0500

    gaivi code

--- run/2023_12_16_15parent_rl_custom.py
+++ run/2023_12_16_15parent_rl_custom.py
@@ -1,6 +1,7 @@
 import time, os, shutil, subprocess, glob, signal
 
 bLocalCarla = False
+bGAIVI = True
 
 # delete files from previous run
 def clean_directory(directory):
@@ -29,21 +30,29 @@ def kill_carla():
 def kill_carla_remote():
     kill_process = subprocess.Popen('ssh SAMBHU23 "killall -9 -r CarlaUE4-Linux"', shell=True)
     kill_process.wait()
+def kill_carla_gaivi():
+    kill_carla()
 
 # check if saved final model exists
 run = 1
 while len(glob.glob('models/final.model')) == 0:
     print(f'Start run at count {run}')
     if bLocalCarla:
-        kill_carla()
+        if not bGAIVI:
+            kill_carla()
+        else:
+            kill_carla_gaivi()
     else:
         kill_carla_remote()
     carla = None
     rl_custom = None
     try:
         if bLocalCarla:
-            carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh -RenderOffScreen', shell=True, preexec_fn=os.setsid)
-            # carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh', shell=True, preexec_fn=os.setsid)
+            if not bGAIVI:
+                carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh -RenderOffScreen', shell=True, preexec_fn=os.setsid)
+                # carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh', shell=True, preexec_fn=os.setsid)
+            else:
+                carla = subprocess.Popen('srun singularity exec --nv /home/n/nsambhu/github/podman-carla/carla-0.9.14.sif /home/carla/CarlaUE4.sh -RenderOffScreen', shell=True, preexec_fn=os.setsid)
         else:
             carla = subprocess.Popen('ssh SAMBHU23 "/opt/carla-simulator/CarlaUE4.sh -RenderOffScreen"', shell=True, preexec_fn=os.setsid)
             time.sleep(30)
