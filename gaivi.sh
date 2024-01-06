53175a867c9f205894f03f42e7a04c30957a43c1
Neil Sambhu <neilsambhu@gmail.com>
2024-01-06 02:02:02 -0500

    batch size A100

--- gaivi.sh
+++ gaivi.sh
@@ -2,11 +2,11 @@
 #All options below are recommended
 #SBATCH -p Contributors #general # run on partition general
 #SBATCH --cpus-per-task=32 # 32 CPUs per task
-#SBATCH --mem=16GB # 100GB per task
-#SBATCH --gpus=8 # 4 GPUs
+#SBATCH --mem=100GB # 100GB per task
+#SBATCH --gpus=3 # 4 GPUs
 ##SBATCH --mail-user=nsambhu@mail.usf.edu # email for notifications
 ##SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE # events for notifications
-##SBATCH -w GPU1
+#SBATCH -w GPU1
 
 #srun podman run -it --privileged -e NVIDIA_VISIBLE_DEVICES=0 --net=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw carlasim/carla:0.9.14 /bin/bash ./CarlaUE4.sh -carla-rpc-port=2000 -RenderOffScreen
 #podman pull carlasim/carla:0.9.14
