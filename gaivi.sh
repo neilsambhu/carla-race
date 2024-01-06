7397b37201557d34c8a4607d2d73fec3302f07a4
Neil Sambhu <neilsambhu@gmail.com>
2024-01-05 23:59:33 -0500

    16 gpus

--- gaivi.sh
+++ gaivi.sh
@@ -3,7 +3,7 @@
 #SBATCH -p Contributors #general # run on partition general
 #SBATCH --cpus-per-task=2 # 32 CPUs per task
 #SBATCH --mem=16GB # 100GB per task
-#SBATCH --gpus=2 # 4 GPUs
+#SBATCH --gpus=16 # 4 GPUs
 ##SBATCH --mail-user=nsambhu@mail.usf.edu # email for notifications
 ##SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE # events for notifications
 ##SBATCH -w GPU17
