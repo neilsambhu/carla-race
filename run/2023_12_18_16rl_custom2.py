4053c13cc2bab4ad1c7e912e426cdc27f6454c6e
Neil Sambhu <neilsambhu@gmail.com>
2024-01-11 14:09:17 -0500

    check save images files to disk

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -246,6 +246,8 @@ class CarEnv:
             os.makedirs('%s/%04d' % (directory_output, self.episode))
             time.sleep(1)
         i4.save('%s/%04d/%06d.png' % (directory_output, self.episode, image.frame))
+        while not os.path.exists('%s/%04d/%06d.png' % (directory_output, self.episode, image.frame)):
+            time.sleep(0.01)
 
     def step(self, action):
         if bVerbose and False:
