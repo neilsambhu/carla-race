60c75edeeb524132584bb38e3477cff50685f73d
Neil Sambhu <neilsambhu@gmail.com>
2024-01-12 14:46:33 -0500

    variable distance reward

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -261,11 +261,11 @@ class CarEnv:
         # You might want to define a threshold and reward scheme based on the distance
         # For example, if distance < threshold: reward = some_value
         # Modify the reward calculation based on your requirements
-        # reward = -1*distance**3 - distance + 1
-        if distance < 10:
-            reward += 10
-        else:
-            reward -= 10
+        reward = -1*distance**3 - distance + 1
+        # if distance < 10:
+        #     reward += 10
+        # else:
+        #     reward -= 10
 
         # Set 'done' flag to True when ticks exceed the lines in the file
         # done = self.idx_tick >= len(lines)
