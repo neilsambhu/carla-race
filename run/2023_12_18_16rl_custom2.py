73e7c525b5dfffe54a1282c016b1e489fd875f98
Neil Sambhu <neilsambhu@gmail.com>
2024-01-13 19:46:52 -0500

    filters 32 to 16

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -330,7 +330,8 @@ with strategy.scope():
             from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Flatten, AveragePooling2D, MaxPooling2D, TimeDistributed
             from tensorflow.keras.models import Model
             input_shape = (COUNT_FRAME_WINDOW, IM_HEIGHT, IM_WIDTH, 3)
-            count_filters = 32
+            # count_filters = 32
+            count_filters = 16
             time_steps = COUNT_FRAME_WINDOW
 
             # Define the input layer
