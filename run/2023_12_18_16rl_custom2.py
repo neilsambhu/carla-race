ba2bfbd4e5311b2e30f08bc9d79848f35848b28e
Neil Sambhu <neilsambhu@gmail.com>
2024-01-31 04:43:24 -0500

    larger minibatch

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -113,7 +113,9 @@ else:
         MINIBATCH_SIZE = 20 # 56 minutes per epoch
         MINIBATCH_SIZE = 20 # 51 minutes per epoch
         MINIBATCH_SIZE = 100 # 47 minutes per epoch (training batch size 20)
-        MINIBATCH_SIZE = 200 # 59 minutes per epoch (training batch size 1)
+        MINIBATCH_SIZE = 200 # 56 minutes per epoch (training batch size 1)
+        MINIBATCH_SIZE = 200 # 47 minutes per epoch (training batch size 20)
+        MINIBATCH_SIZE = 400
 
 # MIN_REPLAY_MEMORY_SIZE = 20_000
 MIN_REPLAY_MEMORY_SIZE = MINIBATCH_SIZE
