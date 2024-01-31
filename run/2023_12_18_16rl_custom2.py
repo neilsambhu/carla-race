5e7d4b6106d016b41a29f59645f0a27728345422
Neil Sambhu <neilsambhu@gmail.com>
2024-01-31 01:14:42 -0500

    syntax for boolean

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -63,7 +63,7 @@ else:
         MINIBATCH_SIZE = REPLAY_MEMORY_SIZE - COUNT_FRAME_WINDOW
         MINIBATCH_SIZE = 1 # 2.3 batches/second on 35 GPUs
         # MINIBATCH_SIZE = 2 #first batch trained; warning
-    else if bGPU43:
+    elif bGPU43:
         # MINIBATCH_SIZE = REPLAY_MEMORY_SIZE // 8192
         # MINIBATCH_SIZE = 1
         # MINIBATCH_SIZE = REPLAY_MEMORY_SIZE // 1024 #failure
