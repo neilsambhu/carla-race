a7dc762ed56d71f6b12b4df68022f60ef691e6d0
Neil Sambhu <neilsambhu@gmail.com>
2024-01-31 03:13:17 -0500

    less memory; 40 GPUs

--- gaivi.sh
+++ gaivi.sh
@@ -4,11 +4,11 @@
 #SBATCH --cpus-per-task=32 # 32 CPUs per task
 ##SBATCH --cpus-per-task=128 # 32 CPUs per task
 ##SBATCH --cpus-per-task=48 # 32 CPUs per task
-#SBATCH --mem=128GB # 100GB per task
+#SBATCH --mem=120GB # 100GB per task
 ##SBATCH --mem=500GB # 100GB per task
 ##SBATCH --mem=250GB # 100GB per task
 ##SBATCH --mem=1900GB # 100GB per task
-#SBATCH --gpus=35 # 63 GPUs available
+#SBATCH --gpus=40 # 63 GPUs available
 #SBATCH --mail-user=nsambhu@mail.usf.edu # email for notifications
 ##SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE # events for notifications
 ##SBATCH --mail-type=END,FAIL,REQUEUE # events for notifications
