8cd6730c05cb8d8bff44b3bba6e395fb7e41160f
Neil Sambhu <neilsambhu@gmail.com>
2024-01-31 23:41:26 -0500

    GPU45

--- gaivi.sh
+++ gaivi.sh
@@ -18,7 +18,7 @@
 ##SBATCH --mail-type=END,FAIL,REQUEUE # events for notifications
 ##SBATCH -w GPU43
 #SBATCH -w GPU45
-#SBATCH -w GPU47
+##SBATCH -w GPU47
 ##SBATCH -w GPU12
 
 #srun podman run -it --privileged -e NVIDIA_VISIBLE_DEVICES=0 --net=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw carlasim/carla:0.9.14 /bin/bash ./CarlaUE4.sh -carla-rpc-port=2000 -RenderOffScreen
