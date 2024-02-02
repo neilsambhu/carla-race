71da4717abe4e2668827d1d722d90a33d6186587
Neil Sambhu <neilsambhu@gmail.com>
2024-02-01 19:06:14 -0500

    single LSTM layer

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -466,8 +466,9 @@ with strategy.scope():
 
 
             # Apply LSTM layer
-            x = Bidirectional(LSTM(units=128, return_sequences=True))(x)
-            x = Bidirectional(LSTM(units=64, return_sequences=False))(x)
+            # x = Bidirectional(LSTM(units=128, return_sequences=True))(x)
+            # x = Bidirectional(LSTM(units=64, return_sequences=False))(x)
+            x = LSTM(units=1024)(x)
             
             # print(f'x.shape after LSTM: {x.shape}')
             size_reduce = 2
