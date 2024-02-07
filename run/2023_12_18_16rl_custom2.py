b7762e4153c24ab1bf8d895ef395d2d4e2927b1c
Neil Sambhu <neilsambhu@gmail.com>
2024-02-07 02:05:46 -0500

    limit 100 frames; further relax early stopping

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -410,8 +410,8 @@ with strategy.scope():
             reward -= distance
 
             # Set 'done' flag to True when ticks exceed the lines in the file
-            done = self.idx_tick >= len(lines)
-            # done = self.idx_tick >= 100
+            # done = self.idx_tick >= len(lines)
+            done = self.idx_tick >= 100
 
             # v = self.vehicle.get_velocity()
             # kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
