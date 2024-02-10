9949710a3c6178492da902ecb244341fe7f237d0
Neil Sambhu <neilsambhu@gmail.com>
2024-02-10 01:39:29 -0500

    use variable frames per episode

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -51,7 +51,7 @@ IM_WIDTH = 128
 IM_HEIGHT = 128
 # SECONDS_PER_EPISODE = 10
 SECONDS_PER_EPISODE = 3*60
-FRAMES_PER_EPISODE = 11
+FRAMES_PER_EPISODE = 10
 # REPLAY_MEMORY_SIZE = 5_000
 # MIN_REPLAY_MEMORY_SIZE = 1_000
 # MIN_REPLAY_MEMORY_SIZE = int(1.5*SECONDS_PER_EPISODE*20) # 12/24/2023 6:37 AM: Neil commented out
@@ -414,7 +414,7 @@ with strategy.scope():
             # Set 'done' flag to True when ticks exceed the lines in the file
             # done = self.idx_tick >= len(lines)
             # done = self.idx_tick >= 100
-            done = self.idx_tick >= 10
+            done = self.idx_tick >= FRAMES_PER_EPISODE
 
             # v = self.vehicle.get_velocity()
             # kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
