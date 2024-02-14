6234361f08166d159610e6ac5f14a1abb12c6610
Neil Sambhu <neilsambhu@gmail.com>
2024-02-14 16:25:38 -0500

    epsilon

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -810,8 +810,8 @@ if __name__ == "__main__":
                         if selected_brake_throttle > 0:
                             brake_value = 0.0
                         print(f'source: {src}\ttick: {env.idx_tick:04d}\tthrottle: {throttle_value:.2f}\tsteer: {steer_value:.2f}\tbrake: {brake_value:.2f}')
-                    # if (np.random.random() > epsilon and count_action_random == 0) or (count_action_model > 0):
-                    if env.idx_tick < FRAMES_PER_EPISODE - (FRAMES_TO_REDO % FRAMES_PER_EPISODE):
+                    if (np.random.random() > epsilon and count_action_random == 0) or (count_action_model > 0):
+                    # if env.idx_tick < FRAMES_PER_EPISODE - (FRAMES_TO_REDO % FRAMES_PER_EPISODE):
                         action = np.argmax(agent.get_qs(np.asarray(window_current_state)))                    
                         print_action('model', action)
                         count_action_model += 1
