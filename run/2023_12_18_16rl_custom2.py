131e86fe1a2e9cb5660d40c226f05e775fb63a9c
Neil Sambhu <neilsambhu@gmail.com>
2024-02-17 00:06:21 -0500

    1 to 28 filters

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -484,9 +484,9 @@ with strategy.scope():
             from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, Activation, Flatten, AveragePooling2D, MaxPooling2D, TimeDistributed, LSTM, Bidirectional
             from tensorflow.keras.models import Model
             input_shape = (COUNT_FRAME_WINDOW, IM_HEIGHT, IM_WIDTH, 3)
-            count_filters = 1 # 2/4/2024 2:47 AM: 73 seconds per epoch
+            # count_filters = 1 # 2/4/2024 2:47 AM: 73 seconds per epoch
+            count_filters = 28
             kernel_size = (1,1)
-            # count_filters = 28
             pool_size = (2,2) # 2/4/2024 2:57 AM: 53 seconds per epoch
             count_lstmNodes = 1024
 
