dd64f6c8cbdf62e7d0887e67b42b4e55fb5e6bfb
Neil Sambhu <neilsambhu@gmail.com>
2024-02-18 11:00:35 -0500

    128 filters; 16 layers

--- run/2023_12_18_16rl_custom2.py
+++ run/2023_12_18_16rl_custom2.py
@@ -489,11 +489,12 @@ with strategy.scope():
             # count_filters = 14 # OOM
             # count_filters = 7 # OOM
             # count_filters = 2
-            count_filters = 28
+            count_filters = 128
             # kernel_size = (1,1)
             kernel_size = (3,3)
             pool_size = (2,2) # 2/4/2024 2:57 AM: 53 seconds per epoch
-            count_layers = 8
+            # count_layers = 8
+            count_layers = 16
             count_lstmNodes = 1024
             # count_lstmNodes = 64
             # count_lstmNodes = 10_000
