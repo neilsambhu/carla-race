d84625f8a19d0ce9b2a0ce6df6a455b9e522d2f9
Neil Sambhu <neilsambhu@gmail.com>
2024-02-21 14:47:21 -0500

    gaivi script

--- gaivi.sh
+++ gaivi.sh
@@ -3,8 +3,8 @@
 #SBATCH -p Contributors
 ##SBATCH -p nopreempt
 
-##SBATCH --cpus-per-task=4
-#SBATCH --cpus-per-task=24 # GPU1
+#SBATCH --cpus-per-task=4
+##SBATCH --cpus-per-task=24 # GPU1
 ##SBATCH --cpus-per-task=8 # GPU13
 ##SBATCH --cpus-per-task=32 # GPU42
 ##SBATCH --cpus-per-task=16 # GPU43
@@ -24,7 +24,7 @@
 ##SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE # events for notifications
 ##SBATCH --mail-type=END,FAIL,REQUEUE # events for notifications
 
-#SBATCH -w GPU1
+##SBATCH -w GPU1
 ##SBATCH -w GPU12
 ##SBATCH -w GPU13
 ##SBATCH -w GPU42
@@ -34,21 +34,6 @@
 ##SBATCH -w GPU47
 ##SBATCH -w GPU12
 
-#srun podman run -it --privileged -e NVIDIA_VISIBLE_DEVICES=0 --net=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw carlasim/carla:0.9.14 /bin/bash ./CarlaUE4.sh -carla-rpc-port=2000 -RenderOffScreen
-#podman pull carlasim/carla:0.9.14
-#srun podman run -d --name carla_container -p 2000-2002:2000-2002 carlasim/carla:0.9.14 ./CarlaUE4.sh -RenderOffScreen
-
-#srun echo 'srun podman run -d --name carla_container -p 2000-2002:2000-2002 docker.io/carlasim/carla:0.9.14 ./CarlaUE4.sh -RenderOffScreen'
-#srun podman run -d --name carla_container -p 2000-2002:2000-2002 docker.io/carlasim/carla:0.9.14 ./CarlaUE4.sh -RenderOffScreen
-#srun nvidia-smi
-
-#singularity pull carla-0.9.14.sif docker://carlasim/carla:0.9.14
-#srun --gpus=1 --pty singularity exec --nv /home/n/nsambhu/github/podman-carla/carla-0.9.14.sif /home/carla/CarlaUE4.sh -RenderOffScreen & nvidia-smi
-# srun singularity exec --nv /home/n/nsambhu/github/podman-carla/carla-0.9.14.sif /home/carla/CarlaUE4.sh -RenderOffScreen &
-# sleep 30
-# nvidia-smi
-# killall -9 -r CarlaUE4-Linux
-# nvidia-smi
-
 conda activate carla_py3.9
-srun python -u run/2023_12_16_15parent_rl_custom.py
+# srun python -u run/2023_12_16_15parent_rl_custom.py
+srun python -u run/2024_02_21_23_parent_rl.py
