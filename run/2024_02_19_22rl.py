fe961fb11cca4c74044d77bce910692f3e4f5023
Neil Sambhu <neilsambhu@gmail.com>
2024-02-21 14:43:52 -0500

    GAIVI

--- run/2024_02_19_22rl.py
+++ run/2024_02_19_22rl.py
@@ -1,27 +1,62 @@
-import carla, time, queue, shutil, os, glob
+import carla, time, queue, shutil, os, glob, math
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+bSAMBHU23 = config.getboolean('Settings','bSAMBHU23')
+bGPU_random = config.getboolean('Settings','bGPU_random')
+bGAIVI = not bSAMBHU24
+
+path_AP_controls = '_out_21_CARLA_AP_Town06/Controls.txt'
+path_AP_locations = '_out_21_CARLA_AP_Town06/Locations.txt'
+
+dir_outptut = '_out_22_rl'
+if os.path.exists(dir_outptut):
+    shutil.rmtree(dir_outptut)
+os.makedirs(dir_outptut)
+dir_output_frames = f'{dir_outptut}/frames/'
+path_rl_controls = f'{dir_outptut}/Controls.txt'
+path_rl_locations = f'{dir_outptut}/Locations.txt'
+
+'''Make sure CARLA Simulator 0.9.14 is running'''
+actor_list = []
 
 def actor_list_destroy(actor_list):
     [x.destroy() for x in actor_list]
     return []
-    
+def getPath_CARLA_AP_Town06():
+    listLocationsPath = []
+    with open(path_AP_locations,'r') as file_AP_locations:
+        for line in file_AP_locations.readlines():
+            lineStripped = line.strip()
+            x,y,z = lineStripped.split()
+            locationFromPath = carla.Location(float(x),float(y),float(z))
+            listLocationsPath.append(locationFromPath)
+    return listLocationsPath
+listLocationsPath = getPath_CARLA_AP_Town06()
+def getLocationClosestToCurrent(currentLocation):
+    distanceMinimum = None
+    listDistance = []
+    for locationFromPath in listLocationsPath:
+        distanceFromPath = currentLocation.distance(locationFromPath)
+        listDistance.append(distanceFromPath)
+    distanceMinimum = min(listDistance)
+    indexMinimum = listDistance.index(distanceMinimum)
+    return listLocationsPath[indexMinimum]
+
 def main():
-    '''Make sure CARLA Simulator 0.9.14 is running'''
-    actor_list = []
-    path_AP_controls = '_out_21_CARLA_AP_Town06/Controls.txt'
-    path_AP_locations = '_out_21_CARLA_AP_Town06/Locations.txt'
-
-    dir_outptut = '_out_22_rl'
-    if os.path.exists(dir_outptut):
-        shutil.rmtree(dir_outptut)
-    os.makedirs(dir_outptut)
-    dir_output_frames = f'{dir_outptut}/frames/'
-    path_rl_controls = f'{dir_outptut}/Controls.txt'
-    path_rl_locations = f'{dir_outptut}/Locations.txt'
-
     try:
         # Connect to the CARLA Simulator
-        client = carla.Client('localhost', 2000)
-        client.set_timeout(20.0)
+        if bSAMBHU23:
+            client = carla.Client('localhost', 2000)
+            client.set_timeout(20.0)
+        if bGAIVI:
+            command_output = subprocess.run(['squeue'], capture_output=True, text=True)
+            output_lines = command_output.stdout.split('\n')
+            carla_line = [line for line in output_lines if 'nsambhu' in line and 'carla.sh' in line and 'GPU' in line]
+            gpu_info = carla_line[-1].split()[-1]  # Assuming GPU info is the last column
+            print("GPU Info for carla.sh:", gpu_info)
+            self.client = carla.Client(gpu_info, 2000)
+            self.client.set_timeout(120)
 
         # Get the world object
         world = client.get_world()
@@ -43,8 +78,9 @@ def main():
         spawn_start_left = carla.Transform(carla.Location(x=19.7, y=240.9, z=height), carla.Rotation())
         spawn_start_center = carla.Transform(carla.Location(x=19.7, y=244.4, z=height), carla.Rotation())
         spawn_start_right = carla.Transform(carla.Location(x=19.7, y=247.9, z=height), carla.Rotation())
-        spawn_destination = carla.Transform(carla.Location(x=581.2, y=244.6, z=height), carla.Rotation())
-        transform = spawn_start_center
+        location_destination = carla.Location(x=581.2, y=244.6, z=height)
+        # transform = spawn_start_center
+        transform = spawn_start_left
 
         # So let's tell the world to spawn the vehicle.
         vehicle = world.spawn_actor(vehicle_bp, transform)
@@ -70,18 +106,51 @@ def main():
         # receives an image. In this example we are saving the image to disk.
         camera.listen(lambda image: image.save_to_disk(f'{dir_output_frames}/%06d.png' % image.frame))
 
-        throttle,steer,brake,hand_brake,reverse,manual_gear_shift,gear=0.5,0.0,0.0,False,False,False,0
         countTick = 0
-        with open(path_rl_locations,'r') as file_rl_locations:
-            for line in file_rl_locations.readlines():
-                lineStripped = line.strip()
-                x,y,z = lineStripped.split()
-                location = carla.Location(float(x),float(y),float(z))
-                world.tick()
-                countTick += 1
-            countTick -= 1
+        world.tick()
+        countTick += 1
+        def getDistanceToDestination():
+            return location_destination.distance(vehicle.get_location())
+        throttle, steer, brake = 1.0, 0.0, 0.0
+        while getDistanceToDestination() > 2:
+            locationClosestToCurrent = getLocationClosestToCurrent(vehicle.get_location())
+            deltaY = locationClosestToCurrent.y - vehicle.get_location().y
+            thresholdDeltaY = 1
+            bWithinThreshold = None
+            unitChangeThrottle = 0.1
+            unitChangeSteer = 0.1
+            unitChangeBrake = 0.1
+            if deltaY >= -thresholdDeltaY and deltaY <= thresholdDeltaY:
+                bWithinThreshold = True
+                throttle, steer, brake = 1.0, 0.0, 0.0
+            elif deltaY < -thresholdDeltaY:
+                bWithinThreshold = False
+                deltaSteer = -unitChangeSteer
+                steer = max(steer+deltaSteer, -0.1)
+            elif deltaY > thresholdDeltaY:
+                bWithinThreshold = False
+                deltaSteer = unitChangeSteer
+                steer = min(steer+deltaSteer, 0.1)
+            if not bWithinThreshold:
+                v = vehicle.get_velocity()
+                kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+                if kmh < 5:
+                    # slow or not moving
+                    brake = 0
+                    deltaThrottle = unitChangeThrottle
+                    throttle = min(throttle+deltaThrottle, 1.0)
+                else:
+                    # already moving
+                    throttle = 0.0
+                    deltaBrake = unitChangeBrake
+                    brake = min(brake+deltaBrake, 1.0)
+            vehicle.apply_control(carla.VehicleControl(throttle=throttle, steer=steer, brake=brake))
+            print(f'Distance to destination: {getDistanceToDestination():.1f}\tdeltaY: {deltaY:.2f}\tthrottle: {throttle:.1f} steer: {steer:.1f} brake: {brake:.1f}')
+            world.tick()
+            countTick += 1
+        countTick -= 1
         while len(glob.glob(os.path.join(dir_output_frames,'*'))) < countTick:
-            time.sleep(1)
+            time.sleep(10)
     finally:
         actor_list_destroy(actor_list)
         print('done')
---
+++ run/2024_02_21_23_parent_rl.py
@@ -0,0 +1,138 @@
+import time, os, shutil, subprocess, glob, signal, configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+bSAMBHU23 = config.getboolean('Settings','bSAMBHU23')
+bLocalCarla = not bSAMBHU23
+bGAIVI = not bSAMBHU23
+# count_max_runs = 1
+count_max_runs = int(1e6)
+pathPythonScript = 'run/2024_02_19_22rl.py'
+# delete files from previous run
+def clean_directory(directory):
+    if not bGAIVI:
+        [os.remove(os.path.join(directory, file)) for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file))]
+        [shutil.rmtree(os.path.join(directory, dir)) for dir in os.listdir(directory) if os.path.isdir(os.path.join(directory, dir))]
+    else:
+        clean = subprocess.Popen(f'rm -rf {directory}/*', shell=True)
+        clean.wait()
+
+start_time = time.time()
+def print_elapsed_time():
+    end_time = time.time()
+    # Calculate elapsed time
+    elapsed_time_seconds = end_time - start_time
+    # Convert seconds to hours, minutes, and seconds
+    hours = int(elapsed_time_seconds // 3600)
+    minutes = int((elapsed_time_seconds % 3600) // 60)
+    seconds = int(elapsed_time_seconds % 60)
+    # Display elapsed time in HH:MM:SS format
+    print(f"Elapsed time: {hours:02}:{minutes:02}:{seconds:02}")
+
+def kill_carla():
+    kill_process = subprocess.Popen('killall -9 -r CarlaUE4-Linux', shell=True)
+    kill_process.wait()
+def kill_carla_remote():
+    kill_process = subprocess.Popen('ssh SAMBHU23 "killall -9 -r CarlaUE4-Linux"', shell=True)
+    kill_process.wait()
+def kill_carla_gaivi():
+    kill_process = subprocess.Popen('scancel --name="carla.sh"', shell=True)
+    kill_process.wait()
+    # time.sleep(10)
+
+# check if saved final model exists
+run = 1
+
+if bLocalCarla:
+    if not bGAIVI:
+        kill_carla()
+    else:
+        # kill_carla_gaivi()
+        pass
+else:
+    kill_carla_remote()
+carla = None
+rl_custom = None
+try:
+    if bLocalCarla:
+        if not bGAIVI:
+            carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh -RenderOffScreen', shell=True, preexec_fn=os.setsid)
+            # carla = subprocess.Popen('/opt/carla-simulator/CarlaUE4.sh', shell=True, preexec_fn=os.setsid)
+        else:
+            print(f"before carla, run squeue")
+            squeue_before_carla = subprocess.Popen('squeue | grep nsambhu', shell=True)
+            squeue_before_carla.wait()
+            # import random
+            # time.sleep(random.uniform(0,60))
+            # if existing CARLA is present, don't spawn CARLA
+            carla_line = []
+            command_output = subprocess.run(['squeue'], capture_output=True, text=True)
+            output_lines = command_output.stdout.split('\n')
+            carla_line = [line for line in output_lines if 'nsambhu' in line and 'carla.sh' in line and not 'CG' in line]
+            if len(carla_line) == 0:
+                carla = subprocess.Popen(f'sbatch /home/n/nsambhu/github/podman-carla/carla.sh ', shell=True)
+                carla.wait()
+                time.sleep(10)
+            print(f"after carla, run squeue")
+            squeue_after_carla = subprocess.Popen('squeue | grep nsambhu', shell=True)
+            squeue_after_carla.wait()
+            carla_line = []
+            while carla_line == []:
+                command_output = subprocess.run(['squeue'], capture_output=True, text=True)
+                output_lines = command_output.stdout.split('\n')
+                carla_line = [line for line in output_lines if 'nsambhu' in line and 'carla.sh' in line and 'GPU' in line and not 'CG' in line]
+            print(f'carla_line: {carla_line}')
+            carla_gpu_info = carla_line[-1].split()[-1]  # Assuming GPU info is the last column
+            print("GPU Info for carla.sh:", carla_gpu_info)
+            import carla
+            # time.sleep(60)
+            world = None
+            count_attempt = 0
+            count_max_attempt = 10
+            while world == None and count_attempt<count_max_attempt:
+                try:
+                    client = carla.Client(str(carla_gpu_info), 2000)
+                    world = client.get_world()
+                    print(f'world: {world}')
+                except Exception as e:
+                    count_attempt+=1
+                    print(f'CARLA starting error message: {e}')
+                    time.sleep(1)
+                    continue
+            if world == None:
+                raise Exception('Code could not connect to CARLA Simulator.')
+            # time.sleep(30)                
+            # nvidia_smi = subprocess.Popen('nvidia-smi', shell=True, preexec_fn=os.setsid)
+            # nvidia_smi.wait()
+    else:
+        carla = subprocess.Popen('ssh SAMBHU23 "/opt/carla-simulator/CarlaUE4.sh -RenderOffScreen"', shell=True, preexec_fn=os.setsid)
+        time.sleep(30)
+    if run == 1:
+        if bSAMBHU23:
+            rl_custom = subprocess.Popen(f'python -u {pathPythonScript} 2>&1 | tee out.txt', shell=True)
+        elif bGAIVI:
+            rl_custom = subprocess.Popen(f'python -u {pathPythonScript}', shell=True)
+    elif run > 1:
+        rl_custom = subprocess.Popen(f'python -u {pathPythonScript}', shell=True)
+    def signal_handler(sig, frame):
+        carla.terminate()
+        rl_custom.terminate()
+    signal.signal(signal.SIGINT, signal_handler)
+    rl_custom.wait()
+except Exception as e:
+    print(f'Run errored at count {run}')
+    print(f'Parent error message: {e}')
+    print(f'Continue to next attempt for run at count {run}')
+    continue
+else:
+    print(f'(Allegedly) no exception occurred for run at count {run}. CARLA Simulator may have crashed.')
+finally:
+    print(f'End run at count {run}')
+    print_elapsed_time()
+    run += 1
+    if bSAMBHU23:
+        carla.terminate()
+    elif bGAIVI:
+        kill_carla_gaivi()
+    rl_custom.terminate()
+print('done');import sys; sys.exit()
