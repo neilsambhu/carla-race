f02bef44e902a3c22be13b72aa7dcf99f24ffdbf
Neil Sambhu <neilsambhu@gmail.com>
2024-04-04 15:04:19 -0400

    lap time results

--- run/2024_04_04_26_parent_rl.py
+++ run/2024_04_04_26_parent_rl.py
@@ -3,7 +3,8 @@ town = subprocess.Popen(['python', 'run/2024_02_19_19town.py'])
 town.wait()
 
 # for speed in [80, 200, 210]:
-for speed in range(30,201,10):
+# for speed in range(30,201,10):
+for speed in range(90,201,10):
     print('--------------------------------------------------')    
     print(f'started run for target speed {speed} km/h')
     driveConstantSpeed = subprocess.Popen(['python', 'run/2024_03_21_25rl.py', '--speed', str(speed)])
