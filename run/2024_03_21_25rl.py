50211c51227db80d2a076616e53bef77e8bc24b0
Neil Sambhu <neilsambhu@gmail.com>
2024-04-04 11:39:00 -0400

    format graph titles

--- run/2024_03_21_25rl.py
+++ run/2024_03_21_25rl.py
@@ -36,7 +36,7 @@ path_rl_locations = f'{dir_outptut}/Locations.txt'
 actor_list = []
 IM_WIDTH = 80*2
 IM_HEIGHT = 60*2
-TARGET_SPEED = 30
+TARGET_SPEED = 45
 
 def actor_list_destroy(actor_list):
     [x.destroy() for x in actor_list]
@@ -204,7 +204,7 @@ def main():
         # ax1.set_ylabel('Delta Y')
         ax1.set_ylabel('Delta Theta')
         # ax1.set_title('Delta Y over Time')
-        ax1.set_title('Delta Theta over Time')
+        ax1.set_title(f'Delta Theta over Time ({TARGET_SPEED} km/h)')
         # Plot setup for overlay
         fig_overlay, ax2 = plt.subplots(figsize=(12, 6))  # Adjust the figsize as needed
         # leg = ax2.legend()
@@ -226,7 +226,7 @@ def main():
         ax2.set_aspect('auto', 'box')
         ax2.set_xlabel('X')
         ax2.set_ylabel('Y')
-        ax2.set_title('Vehicle Location and Path Overlay')
+        ax2.set_title(f'Vehicle Location and Path Overlay ({TARGET_SPEED} km/h)')
         def printLocations(currentLocation, closestLocation):
             return f'current location: {strLocation2D(currentLocation)} | closest location from path: {strLocation2D(closestLocation)}'
         dictLocationPrediction = {}
@@ -426,7 +426,7 @@ def main():
         # ax1.plot(listDeltaY)
         ax1.plot(listDeltaTheta)
         # fig_deltaY.savefig(os.path.join(dir_outptut, 'deltaY.png'))
-        fig_deltaTheta.savefig(os.path.join(dir_outptut, f'deltaTheta{TARGET_SPEEDTARGET_SPEED:03d}.png'))
+        fig_deltaTheta.savefig(os.path.join(dir_outptut, f'deltaTheta{TARGET_SPEED:03d}.png'))
         # plt.close(fig_deltaY)
         plt.close(fig_deltaTheta)
         # Save the overlay plot
@@ -443,12 +443,12 @@ def main():
         ax2.legend()
         ax2.set_xlabel('X')
         ax2.set_ylabel('Y')
-        ax2.set_title('Vehicle Location and Path Overlay')
+        ax2.set_title(f'Vehicle Location and Path Overlay ({TARGET_SPEED} km/h)')
         fig_overlay.savefig(os.path.join(dir_outptut, f'overlay_plot{TARGET_SPEED:03d}.png'))
         plt.close(fig_overlay)
 
         elapsedTime = elapsedSecondsEnd - elapsedSecondsStart
-        def TimeToTextFile(seconds):
+        def TimeToTextFile(elapsed_time_seconds):
             fileTime = os.path.join(
                 dir_outptut, 
                 f'{TARGET_SPEED:03d}_{elapsed_time_seconds:.2f}'
