a8aa8bbfe43b178efcb5b9975c2692bd0aaa335f
Neil Sambhu <neilsambhu@gmail.com>
2024-04-09 16:07:59 -0400

    vehicle name

--- run/2024_03_21_25rl.py
+++ run/2024_03_21_25rl.py
@@ -36,13 +36,17 @@ argparser.add_argument(
     '-d', '--steerDenominator',
     default='50',
     help='Value by which to divide the steering angle')
+argparser.add_argument(
+    '-v', '--vehicle',
+    default='vehicle.tesla.model3',
+    help='Blueprint ID')
 args = argparser.parse_args()
 TARGET_SPEED = int(args.speed)
 
 dir_outptut = '_out_25_rl'
 if not os.path.exists(dir_outptut):
     os.makedirs(dir_outptut)
-dir_output_frames = f'{dir_outptut}/{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_frames/'
+dir_output_frames = f'{dir_outptut}/{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{args.vehicle}_frames/'
 if not os.path.exists(dir_output_frames):
     os.makedirs(dir_output_frames)
 clean_directory(dir_output_frames)
@@ -138,7 +142,7 @@ def main():
 
         # Define the blueprint of the vehicle you want to spawn
         blueprint_library = world.get_blueprint_library()
-        vehicle_bp = blueprint_library.find('vehicle.tesla.model3')
+        vehicle_bp = blueprint_library.find(args.vehicle)
 
         # Now we need to give an initial transform to the vehicle. We choose a
         # random transform from the list of recommended spawn points of the map.
@@ -213,13 +217,13 @@ def main():
         ax0.autoscale_view('tight')
         ax0.set_xlabel('Time-Steps')
         ax0.set_ylabel('Distance from Predicted Location to Path')
-        ax0.set_title(f'Distance of Deviation From Path ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator)')
+        ax0.set_title(f'Distance of Deviation From Path ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator, {args.vehicle})')
         fig_deltaTheta, ax1 = plt.subplots(figsize=(12, 6))
         ax1.set_xlabel('Time-Steps')
         # ax1.set_ylabel('Delta Y')
         ax1.set_ylabel('Delta Theta')
         # ax1.set_title('Delta Y over Time')
-        ax1.set_title(f'Delta Theta over Time ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator)')
+        ax1.set_title(f'Delta Theta over Time ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator, {args.vehicle})')
         # Plot setup for overlay
         fig_overlay, ax2 = plt.subplots(figsize=(12, 6))  # Adjust the figsize as needed
         # leg = ax2.legend()
@@ -241,7 +245,7 @@ def main():
         ax2.set_aspect('auto', 'box')
         ax2.set_xlabel('X')
         ax2.set_ylabel('Y')
-        ax2.set_title(f'Vehicle Location and Path Overlay ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator)')
+        ax2.set_title(f'Vehicle Location and Path Overlay ({TARGET_SPEED} km/h, {int(args.steerDenominator):03d} steer denominator, {args.vehicle})')
         def printLocations(currentLocation, closestLocation):
             return f'current location: {strLocation2D(currentLocation)} | closest location from path: {strLocation2D(closestLocation)}'
         dictLocationPrediction = {}
@@ -443,11 +447,11 @@ def main():
         elapsedSecondsEnd = world.get_snapshot().timestamp.elapsed_seconds
         # Save the delta Y plot
         ax0.plot(listDistancePredToPath)
-        fig_distancePredToPath.savefig(os.path.join(dir_outptut, f'distancePredToPath{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}.png'))
+        fig_distancePredToPath.savefig(os.path.join(dir_outptut, f'distancePredToPath{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{args.vehicle}.png'))
         # ax1.plot(listDeltaY)
         ax1.plot(listDeltaTheta)
         # fig_deltaY.savefig(os.path.join(dir_outptut, 'deltaY.png'))
-        fig_deltaTheta.savefig(os.path.join(dir_outptut, f'deltaTheta{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}.png'))
+        fig_deltaTheta.savefig(os.path.join(dir_outptut, f'deltaTheta{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{args.vehicle}.png'))
         # plt.close(fig_deltaY)
         plt.close(fig_deltaTheta)
         # Save the overlay plot
@@ -466,14 +470,14 @@ def main():
         ax2.set_ylabel('Y')
         # ax2.set_title(f'Vehicle Location and Path Overlay ({TARGET_SPEED} km/h)')
         plt.rcParams.update({'font.size': 24})
-        fig_overlay.savefig(os.path.join(dir_outptut, f'overlay_plot{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}.png'))
+        fig_overlay.savefig(os.path.join(dir_outptut, f'overlay_plot{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{args.vehicle}.png'))
         plt.close(fig_overlay)
 
         elapsedTime = elapsedSecondsEnd - elapsedSecondsStart
         def TimeToTextFile(elapsed_time_seconds):
             fileTime = os.path.join(
                 dir_outptut, 
-                f'{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{elapsed_time_seconds:.2f}'
+                f'{TARGET_SPEED:03d}_{int(args.steerDenominator):03d}_{args.vehicle}_{elapsed_time_seconds:.2f}'
             )
             open(fileTime,'w')
         TimeToTextFile(elapsedTime)
