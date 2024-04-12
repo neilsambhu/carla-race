da793a5d6df4b212b83591f7bf87f83d451aab60
Neil Sambhu <neilsambhu@gmail.com>
2024-04-12 14:08:14 -0400

    min speed tuned

--- run/2024_03_21_25rl.py
+++ run/2024_03_21_25rl.py
@@ -369,7 +369,7 @@ def main():
             # thresholdDeltaThetaNoSteer = 0.5e-10
             thresholdDeltaThetaNoSteer = 5
             thresholdDeltaThetaSteer = 1e-1
-            speedMinimum = 5
+            speedMinimum = 20
             speedTarget = TARGET_SPEED
             bWithinThreshold = None
             maxSteer = None
