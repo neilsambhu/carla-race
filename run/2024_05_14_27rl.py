16fb1756881cca02978497cf6ad03af1f7ab27a3
Neil Sambhu <neilsambhu@gmail.com>
2024-05-25 06:58:30 -0400

    speed threshold 1e-5

--- run/2024_05_14_27rl.py
+++ run/2024_05_14_27rl.py
@@ -384,7 +384,7 @@ def main():
                 # thresholdDeltaThetaNoSteer = 0.5e-10
                 thresholdDeltaThetaNoSteer = 5
                 thresholdDeltaThetaSteer = 1e-1
-                speedMinimum = 1e-3
+                speedMinimum = 1e-5
                 speedTarget = TARGET_SPEED
                 bWithinThreshold = None
                 maxSteer = None
