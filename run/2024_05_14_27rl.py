d51e9d4de8d88d1f97e069c38f289733fdf63787
Neil Sambhu <neilsambhu@gmail.com>
2024-05-27 14:06:18 -0400

    distance threshold 0.1 to 0.01

--- run/2024_05_14_27rl.py
+++ run/2024_05_14_27rl.py
@@ -491,7 +491,7 @@ def main():
                 )
             def GetVehicleControlsGraph(locationCurrent, bMetSpeedMinimum, countTicksNotMoving):
                 listLocations.append(vehicle.get_location())
-                distanceThreshold = 0.1
+                distanceThreshold = 0.01
                 bLookupSuccess = False
                 closestNodeIdx = len(node_locations)
                 speedMinimum = 1e-5
